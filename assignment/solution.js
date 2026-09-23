e210364876d58f6dd8f070b44c5b959ba01fc15b
saad <saad.aboshala@student.giu-uni.de>
2026-09-23 12:28:06 +0200

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "saad yaser the Warrior";
+const studentId = 16020507;
+const power = studentId * 9;
+const warcry = "By the Giza plateau, the Griffin shatters!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
